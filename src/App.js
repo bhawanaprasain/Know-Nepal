7e3db3b39cb4d954259ac4aa619c51db57aea6b0
bhawana <bhawana.prs@gmail.com>
2020-05-04 14:50:25 +0545

    Routing for quiz

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Play from "./components/quiz/Play"
+import Home from "./components/Home"
+import QuizSummary from "./components/quiz/QuizSummary"
+import QuizInstructions from "./components/quiz/QuizInstructions"
+import {BrowserRouter as Router, Route} from "react-router-dom"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Route path="/" exact component={Home} />
+      <Route path="/playnow/instruction" component={QuizInstructions} />
+      <Route path="/play" exact component={Play} />
+      <Route path="/play/quizsummary" exact component={QuizSummary } />
+    </Router>
   );
 }
 

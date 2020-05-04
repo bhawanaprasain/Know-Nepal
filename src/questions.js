2ec74a106dfd808d5fb313177524d27e56e0dfeb
bhawana <bhawana.prs@gmail.com>
2020-05-04 14:51:01 +0545

    Questions for quiz

---
+++ src/questions.js
@@ -0,0 +1,138 @@
+const questions =[
+    {
+      "question": "Where is the tallest bridge of Nepal?",
+      "optionA": "Manthali, Ramechhap",
+      "optionB": "Jiri, Dolakha",
+      "optionC": "Modikhola, Parvat",
+      "optionD": "Sindhuli",
+      "answer": "Modikhola, Parvat"
+    },
+  
+    {
+      "question": "What portion of the world is occupied by the wetland?",
+      "optionA": "5.9%",
+      "optionB": "6.2% ",
+      "optionC": "10%",
+      "optionD": "3%",
+      "answer": "6.2% "
+    },
+  
+    {
+      "question": "How many places of Nepal have been declared wetlands (simsar) now?",
+      "optionA": "Six",
+      "optionB": "Seven",
+      "optionC": "Eight",
+      "optionD": "Nine",
+      "answer": "Nine"
+    },
+  
+    {
+      "question": "When did Tul Bahadur Pun, a Victoria Cross winner, pass away?",
+      "optionA": "9 Baishakh, 2068 ",
+      "optionB": "7 Baishakh, 2068 ",
+      "optionC": "26 Baishakh, 2068 ",
+      "optionD": "12 Baishakh, 2068 ",
+      "answer": "7 Baishakh, 2068 "
+    },
+  
+    {
+      "question": "What has the government of Nepal declared to observe the year 2010 AD as?",
+      "optionA": "violence against dowary",
+      "optionB": "violence against sex",
+      "optionC": "violence against murder",
+      "optionD": "violence against crime",
+      "answer": "violence against sex"
+    },
+  
+    {
+      "question": "Who is the artist to create world record by filling colour on a 123 m long canvas on a single day?",
+      "optionA": "Tilochan Gautam",
+      "optionB": "Rabindra Manandhar",
+      "optionC": "Tilak Manandhar",
+      "optionD": "Kiran Manandhar",
+      "answer": "Kiran Manandhar"
+    },
+  
+    {
+      "question": "With an objective of conserving the culture and tradition of the Tamang people, where was the Tamang Museum established?",
+      "optionA": "Taplejung",
+      "optionB": "Rasuwa",
+      "optionC": "Kaski",
+      "optionD": "Mananag",
+      "answer": "Rasuwa"
+    },
+  
+    {
+      "question": "Which is the conservation area first established in Nepal?",
+      "optionA": "Annapurna Conservation Area",
+      "optionB": "Makalu Barun Conservation Area",
+      "optionC": "Koshi Tappu wildlife",
+      "optionD": "Chitwan National Park",
+      "answer": "Annapurna Conservation Area"
+    },
+  
+    {
+      "question": "When was the Amali system eradicated?",
+      "optionA": "1964 BS",
+      "optionB": "1968 BS",
+      "optionC": "1963 BS",
+      "optionD": "1965 BS",
+      "answer": "1968 BS"
+    },
+  
+    {
+      "question": "From which  did music originate?",
+      "optionA": "Yajur Ved",
+      "optionB": "Rigz Ved",
+      "optionC": "Sama Ved",
+      "optionD": "Ramayan",
+      "answer": "Sama Ved"
+    },
+  
+    {
+      "question": "What’s the most important book in the Muslim religion?",
+      "optionA": "The Koran",
+      "optionB": "The Dictionary",
+      "optionC": "The Bible",
+      "optionD": "The Chemistry text Book",
+      "answer": "The Koran"
+    },
+  
+    {
+      "question": "What’s the capital of Ethiopia?",
+      "optionA": "Cape Town",
+      "optionB": "San Francisco",
+      "optionC": "Abuja",
+      "optionD": "Syndey",
+      "answer": "Addis Ababa"
+    },
+  
+    {
+      "question": "Into how many categories is the land classified on the basis of productivity?",
+      "optionA": "5",
+      "optionB": "4",
+      "optionC": "6",
+      "optionD": "2",
+      "answer": "4"
+    },
+  
+    {
+      "question": "Who was the father of Rana Prime Minister Mohan Shumsher?",
+      "optionA": "Chandra Shumsher",
+      "optionB": "Juddha Shumsher",
+      "optionC": "Dev Shumsher",
+      "optionD": "Padma Shumsher",
+      "answer": "Chandra Shumsher"
+    },
+  
+    {
+      "question": "According to the Muluki Ain 2020, what was the maximum interest rate allowed in Nepal?",
+      "optionA": "14%",
+      "optionB": "13%",
+      "optionC": "10%",
+      "optionD": "12%",
+      "answer": "10%"
+    }
+  ]
+
+  export default questions
